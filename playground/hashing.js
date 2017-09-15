977b527d6484395202d3407e0631ee0f4ed69af0
Ryan Hollander <ryanhollander@ryan-hollanders-macbook-pro.local>
2017-09-15 02:11:58 -0400

    Add Post /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,43 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
+
+// What server sends to client
+var data = {
+  id: 4
+};
+
+// What client sends to server
+var token = {
+  data,
+  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+}
+
+// Man in the middle manipulating data
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// Stores the hash of the data that comes back
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// Check if data was changed.
+if (resultHash === token.hash) {
+  console.log("Data was not changed.  You can trust it.");
+} else {
+  console.log("Data was changed.  Do not trust!");
+}
