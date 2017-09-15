5c7d81c355a85e97f94cad2460b699caf2465acf
Ryan Hollander <ryanhollander@ryan-hollanders-macbook-pro.local>
2017-09-14 21:53:58 -0400

    Fixed semicolon

--- server/server.js
+++ server/server.js
@@ -1,13 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
-console.log('env *****', env);
-
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+require('./config/config');
 
 const lodash = require('lodash');
 const express = require('express');
@@ -17,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
