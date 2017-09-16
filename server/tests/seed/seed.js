4f73b5604b68ee7d70460119211e5c874bcd3789
Ryan Hollander <ryanhollander@ryan-hollanders-macbook-pro.local>
2017-09-15 22:36:34 -0400

    Updated todo routes to make them private

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,33 +1,39 @@
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
 const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
-const jwt = require('jsonwebtoken');
 
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
 const users = [{
-  _id: new ObjectID,
+  _id: userOneId,
   email: 'andrew@example.com',
   password: 'userOnePass',
-  tokens: {
+  tokens: [{
     access: 'auth',
     token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
-  }
+  }]
 }, {
-  _id: new ObjectID,
+  _id: userTwoId,
   email: 'jen@example.com',
   password: 'userTwoPass',
-}]
-
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
+}];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
@@ -36,19 +42,13 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-
-// This function populates the test database with Users seed data above
 const populateUsers = (done) => {
-  // Pass in empty object to remove every record
   User.remove({}).then(() => {
-    // Then save the two users in the seed data that we created above
     var userOne = new User(users[0]).save();
     var userTwo = new User(users[1]).save();
 
-    // Wait for all of the promises to succeed in the array of promises that are passed in - userOne and userTwo.  This will not fire until all of the promises are resolved.  When that happens, we will return these Promise.all result.
     return Promise.all([userOne, userTwo])
-    // Then call done to wrap up the middlewear function
   }).then(() => done());
-}
+};
 
 module.exports = {todos, populateTodos, users, populateUsers};
